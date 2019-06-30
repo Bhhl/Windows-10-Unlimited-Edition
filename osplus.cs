4700b8b90cc42530b71f5fa79361ff8268386ce9
Bhhl <51732503+Bhhl@users.noreply.github.com>
2019-06-30 17:13:12 +1000

    Add files via upload

---
+++ osplus.cs
@@ -0,0 +1,147 @@
+debug.console();
+debug.compile();
+ReandLn(a,b,c);
+WriteLn(a,b,c);
+unit library();
+unit main();
+unit cloud();
+as.integer();
+//Windows 10 using cloud performance
+//Windows 10 using cloud sky
+//Windows 10 using cloud RAM
+//Windows 10 using cloud CPU
+//Windows 10 using cloud Hardware
+//Windows 10 using cloud GPU
+//Windows 10, RAM using cloud performance
+//Windows 10, CPU using cloud performance
+//Windows 10, GPU using cloud performance
+//Windows 10, Hardware using cloud performance
+//Windows 10, cloudsky
+//Windows 10 using cloud videomemory
+//Windows 10, videomemory using cloud performance
+//Windows 10, using cloud optimization
+//Windows 10, RAM using cloud optimization
+//Windows 10, CPU using cloud optimization
+//Windows 10, GPU using cloud optimization
+//Windows 10, Hardware using cloud optimization
+//Windows 10, Videomemory using cloud optimization
+//Windows 10 using cloud Wi-Fi
+//Windows 10 using cloud Wi-Fi speed
+//Windows 10 using cloud Wi-Fi connection
+//Windows 10 using cloud Wi-Fi UDP and TCP
+//Windows 10, Wi-Fi speed using cloud performance
+//Windows 10, Wi-Fi connection using cloud performance
+//Windows 10, Wi-Fi UDP and TCP using cloud performance
+//Windows 10, Wi-Fi using cloud performance
+//Windows 10, Wi-Fi speed using cloud optimization
+//Windows 10, Wi-Fi connection using cloud optimization
+//Windows 10, Wi-Fi UDP and TCP using cloud optimization
+//Windows 10, Wi-Fi using cloud optimization 
+//Windows 10, Wi-Fi ping using cloud performance
+//Windows 10, Wi-Fi ping using cloud optimization
+//Windows 10, using Net cloud
+//Windows 10, Net using cloud speed
+//Windows 10, Net using cloud ping
+//Windows 10, Net using cloud performance
+//Windows 10, Net using cloud optimization
+//Windows 10, Net using cloud TCP and UDP
+//Windows 10, Net using cloud Protocol
+//Windows 10, Net using cloud https
+//Windows 10, Net using cloud http
+//Windows 10, Net using cloud OSI
+//Windows 10 using cloud toolbar
+//Windows 10 using cloud Desktop
+//Windows 10 using cloud Start Menu
+//Windows 10 using cloud Context
+//Windows 10 using cloud Context menu
+//Windows 10 using cloud settings
+//Windows 10 using cloud properties
+//Windows 10 using cloud exe
+//Windows 10 using cloud .exe
+//Windows 10 using cloud executable
+//Windows 10 using cloud framerate
+//Windows 10 using cloud fps
+//Windows 10 using cloud Ethernet
+//Windows 10 using cloud Ethernet speed
+//Windows 10 using cloud Ethernet ping
+//Windows 10 using cloud Ethernet connection
+//Windows 10 using cloud Ethernet UDP and TCP
+//Windows 10 using cloud Wi-Fi signal
+//Windows 10 using cloud applications
+//Windows 10 using cloud Explorer
+//Windows 10 using cloud DirectX
+//Windows 10 using cloud Direct3D
+//Windows 10 using cloud OpenGl
+//Windows 10 using cloud wallpapers
+//Windows 10 using cloud resolution
+//Windows 10 using cloud screen resolution
+//Windows 10 using cloud screen quality
+//Windows 10 using cloud screen definition
+//Windows 10 using cloud fonts
+//Windows 10 using cloud fonts quality
+//Windows 10 using cloud fonts definition
+//Windows 10 using cloud screen visible
+//Windows 10 using cloud fonts visible
+//Windows 10 using cloud Net images
+//Windows 10 using cloud Net videos
+//Windows 10 using cloud Net visible
+//Windows 10 using cloud Net
+//Windows 10 using cloud LED
+//Windows 10 using cloud screen colors
+//Windows 10 using cloud LED lighting
+//Windows 10 using cloud LED visible
+//Windows 10 using cloud sound
+//Windows 10 using cloud audio
+//Windows 10 using cloud images
+//Windows 10 using cloud videos
+//Windows 10 using cloud visible
+//Windows 10 using cloud Keyboard
+//Windows 10 using cloud Mouse
+//Windows 10 using cloud
+//Windows 10 using cloud read and write files
+//Windows 10 using cloud read and write information
+//Windows 10 using cloud speed
+//Windows 10 using cloud connection
+//Windows 10 using cloud ping
+//Windows 10 using cloud Net images quality
+//Windows 10 using cloud Net images definition
+//Windows 10 using cloud Net images visible
+//Windows 10 using cloud Net videos quality
+//Windows 10 using cloud Net videos definition
+//Windows 10 using cloud Net videos visible
+//Windows 10 using cloud images quality
+//Windows 10 using cloud images definition
+//Windows 10 using cloud images visible
+//Windows 10 using cloud videos quality
+//Windows 10 using cloud videos definition
+//Windows 10 using cloud videos visible
+//Windows 10 using cloud Net media
+//Windows 10 using cloud Net media quality
+//Windows 10 using cloud Net media definition
+//Windows 10 using cloud Net media visible
+//Windows 10 using cloud Net media resolution
+//Windows 10 using cloud media quality
+//Windows 10 using cloud media definition
+//Windows 10 using cloud media visible
+//Windows 10 using cloud media resolution
+//Windows 10 using cloud media
+//Windows 10 using cloud images resolution
+//Windows 10 using cloud videos resolution
+//Windows 10 using cloud Net videos resolution
+//Windows 10 using cloud Net images resolution
+//Windows 10 using cloud Net html resolution
+//Windows 10 using cloud Net html quality
+//Windows 10 using cloud Net html definition
+//Windows 10 using cloud Net html visible
+//Windows 10 using cloud Net https
+//Windows 10 using cloud Net https resolution
+//Windows 10 using cloud Net https definition
+//Windows 10 using cloud Net https quality
+//Windows 10 using cloud Net https visible
+//Windows 10 using cloud LED quality
+//Windows 10 using cloud LED definition
+//Windows 10 using cloud LED resolution
+//Windows 10 using cloud LED
+//Windows 10 using cloud Power block
+//Windows 10 using cloud Processor
+//Windows 10 using cloud Memory
