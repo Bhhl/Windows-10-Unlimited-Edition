0cbf65b554ba5a92a865f451fe8d0e4c60a19c6e
Bhhl <51732503+Bhhl@users.noreply.github.com>
2019-07-20 13:03:38 +1000

    Add files via upload

---
+++ osplus.c
@@ -0,0 +1,847 @@
+int sum = 0;
+int sum = 1;
+int sum = 0;
+int sum = 1;
+
+//Windows 10 optimizing performance
+int sum = 0;
+int sum = 1;
+int sum = 0;
+int sum = 1;
+int sum = 0;
+int sum = 1;
+int sum = 0;
+int sum = 1;
+
+//Windows 10 optimizing toolbar
+int sum = 0;
+int sum = 1;
+int sum = 0;
+int sum = 1;
+int sum = 0;
+int sum = 1;
+int sum = 0;
+int sum = 1;
+int sum = 0;
+int sum = 1;
+
+//Windows 10 optimizing start menu
+int sum = 0;
+int sum = 1;
+int sum = 0;
+int sum = 1;
+int sum = 0;
+int sum = 1;
+int sum = 0;
+int sum = 1;
+int sum = 0;
+int sum = 1;
+
+//Windows 10 optimizing CPU performance
+int sum = 0;
+int sum = 1;
+int sum = 0;
+int sum = 1;
+int sum = 0;
+int sum = 1;
+int sum = 0;
+int sum = 1;
+int sum = 0;
+int sum = 1;
+
+//Windows 10 optimizing GPU performance
+int sum = 0;
+int sum = 1;
+int sum = 0;
+int sum = 1;
+int sum = 0;
+int sum = 1;
+int sum = 0;
+int sum = 1;
+
+//Windows 10 optimizing RAM performance
+int sum = 0;
+int sum = 1;
+int sum = 0;
+int sum = 1;
+int sum = 0;
+int sum = 1;
+int sum = 0;
+int sum = 1;
+int sum = 0;
+int sum = 1;
+
+//Windows 10 optimizing fixer hight CPU load
+int sum = 0;
+int sum = 1;
+int sum = 0;
+int sum = 1;
+int sum = 0;
+int sum = 1;
+int sum = 0;
+int sum = 1;
+int sum = 0;
+int sum = 1;
+
+//Windows 10 optimizing fixer hight GPU load
+int sum = 0;
+int sum = 1;
+int sum = 0;
+int sum = 1;
+int sum = 0;
+int sum = 1;
+int sum = 0;
+int sum = 1;
+int sum = 0;
+int sum = 1;
+
+//Windows 10 optimizing fixer hight RAM load
+int sum = 0;
+int sum = 1;
+int sum = 0;
+int sum = 1;
+int sum = 0;
+int sum = 1;
+int sum = 0;
+int sum = 1;
+int sum = 0;
+int sum = 1;
+
+//Windows 10 optimizing Videomemory
+int sum = 0;
+int sum = 1;
+int sum = 0;
+int sum = 1;
+int sum = 0;
+int sum = 1;
+int sum = 0;
+int sum = 1;
+int sum = 0;
+int sum = 1;
+
+//Windows 10 optimizing fixer Videomemory load
+int sum = 0;
+int sum = 1;
+int sum = 0;
+int sum = 1;
+int sum = 0;
+int sum = 1;
+int sum = 0;
+int sum = 1;
+int sum = 0;
+int sum = 1;
+
+//Windows 10 transform 512mb videomemory into 4096mb videomemory
+int sum = 0;
+int sum = 1;
+int sum = 0;
+int sum = 1;
+int sum = 0;
+int sum = 1;
+int sum = 0;
+int sum = 1;
+int sum = 0;
+int sum = 1;
+
+//Windows 10 transform 2048mb RAM into 8192mb RAM
+int sum = 0;
+int sum = 1;
+int sum = 0;
+int sum = 1;
+int sum = 0;
+int sum = 1;
+int sum = 0;
+int sum = 1;
+int sum = 0;
+int sum = 1;
+int sum = 0;
+int sum = 1;
+
+//Windows 10 transform low performance into ultra hight performance
+int sum = 0;
+int sum = 1;
+int sum = 0;
+int sum = 1;
+int sum = 0;
+int sum = 1;
+int sum = 0;
+int sum = 1;
+int sum = 0;
+int sum = 1;
+int sum = 0;
+int sum = 1;
+
+//Windows 10 transform low quality screen into ultra hight quality screen
+int sum = 0;
+int sum = 1;
+int sum = 0;
+int sum = 1;
+int sum = 0;
+int sum = 1;
+int sum = 0;
+int sum = 1;
+int sum = 0;
+int sum = 1;
+int sum = 0;
+int sum = 1;
+int sum = 0;
+int sum = 1;
+
+//Windows 10 transform low screen DPI into Ultra hight screen DPI
+int sum = 0;
+int sum = 1;
+int sum = 0;
+int sum = 1;
+int sum = 0;
+int sum = 1;
+int sum = 0;
+int sum = 1;
+int sum = 0;
+int sum = 1;
+int sum = 0;
+int sum = 1;
+
+//Windows 10 transform low DV screen quality to ultra hight DV screen quality
+int sum = 0;
+int sum = 1;
+int sum = 0;
+int sum = 1;
+int sum = 0;
+int sum = 1;
+int sum = 0;
+int sum = 1;
+int sum = 0;
+int sum = 1;
+
+//Windows 10 transform Wifi speed 0kbps into 1000000kbps Wifi speed
+int sum = 0;
+int sum = 1;
+int sum = 0;
+int sum = 1;
+int sum = 0;
+int sum = 1;
+int sum = 0;
+int sum = 1;
+int sum = 0;
+int sum = 1;
+int sum = 0;
+int sum = 1;
+
+//Windows 10 transform Wifi speed 0kb/s into 1000000kbps Wifi speed
+int sum = 0;
+int sum = 1;
+int sum = 0;
+int sum = 1;
+int sum = 0;
+int sum = 1;
+int sum = 0;
+int sum = 1;
+int sum = 0;
+int sum = 1;
+int sum = 0;
+int sum = 1;
+int sum = 0;
+int sum = 1;
+
+//Windows 10 transform low Wifi signal into hight Wifi signal
+int sum = 0;
+int sum = 1;
+int sum = 0;
+int sum = 1;
+int sum = 0;
+int sum = 1;
+int sum = 0;
+int sum = 1;
+int sum = 0;
+int sum = 1;
+int sum = 0;
+int sum = 1;
+
+//Windows 10 transform medium Wifi signal into hight Wifi signal
+int sum = 0;
+int sum = 1;
+int sum = 0;
+int sum = 1;
+int sum = 0;
+int sum = 1;
+int sum = 0;
+int sum = 1;
+int sum = 0;
+int sum = 1;
+int sum = 0;
+int sum = 1;
+int sum = 0;
+int sum = 1;
+
+//Windows 10 transform medium performance to ultra hight performance
+int sum = 0;
+int sum = 1;
+int sum = 0;
+int sum = 1;
+int sum = 0;
+int sum = 1;
+int sum = 0;
+int sum = 1;
+int sum = 0;
+int sum = 1;
+int sum = 0;
+int sum = 1;
+int sum = 0;
+int sum = 1;
+int sum = 0;
+int sum = 1;
+
+//Windows 10 transform hight performance to ultra hight performance
+int sum = 0;
+int sum = 1;
+int sum = 0;
+int sum = 1;
+int sum = 0;
+int sum = 1;
+int sum = 0;
+int sum = 1;
+int sum = 0;
+int sum = 1;
+int sum = 0;
+int sum = 0;
+int sum = 1;
+int sum = 0;
+int sum = 1;
+int sum = 0;
+int sum = 1;
+
+//Windows 10 transform low screen color gamma into hight screen color gamma
+int sum = 0;
+int sum = 1;
+int sum = 0;
+int sum = 1;
+int sum = 0;
+int sum = 1;
+int sum = 0;
+int sum = 1;
+int sum = 0;
+int sum = 1;
+int sum = 0;
+int sum = 1;
+int sum = 0;
+int sum = 1;
+int sum = 0;
+int sum = 1;
+
+//Windows 10 transform Net to Ultra Net
+int sum = 0;
+int sum = 1;
+int sum = 0;
+int sum = 1;
+int sum = 0;
+int sum = 1;
+int sum = 0;
+int sum = 1;
+int sum = 0;
+int sum = 1;
+int sum = 0;
+int sum = 1;
+
+//Windows 10 transform Net low speed to Hight Net speed
+int sum = 0;
+int sum = 1;
+int sum = 0;
+int sum = 1;
+int sum = 0;
+int sum = 1;
+int sum = 0;
+int sum = 1;
+int sum = 0;
+int sum = 1;
+int sum = 0;
+int sum = 1;
+
+//Windows 10 transform Net medium speed to Net hight speed
+int sum = 0;
+int sum = 1;
+int sum = 0;
+int sum = 1;
+int sum = 0;
+int sum = 1;
+int sum = 0;
+int sum = 1;
+int sum = 0;
+int sum = 1;
+int sum = 0;
+int sum = 1;
+
+//Windows 10 transform Net hight speed to Net ultra hight speed
+int sum = 0;
+int sum = 1;
+int sum = 0;
+int sum = 1;
+int sum = 0;
+int sum = 1;
+int sum = 0;
+int sum = 1;
+int sum = 0;
+int sum = 1;
+int sum = 0;
+int sum = 1;
+
+//Windows 10 transform Net hight ping into Net low ping
+int sum = 0;
+int sum = 1;
+int sum = 0;
+int sum = 1;
+int sum = 0;
+int sum = 1;
+int sum = 0;
+int sum = 1;
+int sum = 0;
+int sum = 1;
+int sum = 0;
+int sum = 1;
+
+//Windows 10 transform Net medium ping to Net low ping
+int sum = 0;
+int sum = 1;
+int sum = 0;
+int sum = 1;
+int sum = 0;
+int sum = 1;
+int sum = 0;
+int sum = 1;
+int sum = 0;
+int sum = 1;
+int sum = 0;
+int sum = 1;
+int sum = 0;
+int sum = 1;
+
+//Windows 10 transform Net ultra hight ping into Net ultra low ping
+int sum = 0;
+int sum = 1;
+int sum = 0;
+int sum = 1;
+int sum = 0;
+int sum = 1;
+int sum = 0;
+int sum = 1;
+int sum = 0;
+int sum = 1;
+int sum = 0;
+int sum = 1;
+
+//Windows 10 transform Net low bandwidth into Net hight bandwidth
+int sum = 0;
+int sum = 1;
+int sum = 0;
+int sum = 1;
+int sum = 0;
+int sum = 1;
+int sum = 0;
+int sum = 1;
+int sum = 0;
+int sum = 1;
+int sum = 0;
+int sum = 1;
+int sum = 0;
+int sum = 1;
+
+//Windows 10 transform Net medium bandwidth into Net hight bandwidth
+int sum = 0;
+int sum = 1;
+int sum = 0;
+int sum = 1;
+int sum = 0;
+int sum = 1;
+int sum = 0;
+int sum = 1;
+int sum = 0;
+int sum = 1;
+int sum = 0;
+int sum = 1;
+int sum = 0;
+int sum = 1;
+
+//Windows 10 transform Net hight bandwidth into Net ultra hight bandwidth
+int sum = 0;
+int sum = 1;
+int sum = 0;
+int sum = 1;
+int sum = 0;
+int sum = 1;
+int sum = 0;
+int sum = 1;
+int sum = 0;
+int sum = 1;
+int sum = 0;
+int sum = 1;
+int sum = 0;
+int sum = 1;
+int sum = 0;
+int sum = 1;
+int sum = 0;
+int sum = 1;
+int sum = 0;
+int sum = 1;
+
+//Windows 10 transform Net low UDP into Net hight UDP
+int sum = 0;
+int sum = 1;
+int sum = 0;
+int sum = 1;
+int sum = 0;
+int sum = 1;
+int sum = 0;
+int sum = 1;
+int sum = 0;
+int sum = 1;
+
+//Windows 10 transform Net medium UDP into Net hight UDP
+int sum = 0;
+int sum = 1;
+int sum = 0;
+int sum = 1;
+int sum = 0;
+int sum = 1;
+int sum = 0;
+int sum = 1;
+int sum = 0;
+int sum = 1;
+
+//Windows 10 transform Net hight UDP into Net Ultra hight UDP
+int sum = 0;
+int sum = 1;
+int sum = 0;
+int sum = 1;
+int sum = 0;
+int sum = 1;
+int sum = 0;
+int sum = 1;
+int sum = 0;
+int sum = 1;
+int sum = 0;
+int sum = 1;
+
+//Windows 10 transform Net low TCP into intro Net hight TCP
+int sum = 0;
+int sum = 1;
+int sum = 0;
+int sum = 1;
+int sum = 0;
+int sum = 1;
+int sum = 0;
+int sum = 1;
+int sum = 0;
+int sum = 1;
+int sum = 0;
+int sum = 1;
+
+//Windows 10 transform Net medium TCP into Net hight TCP
+int sum = 0;
+int sum = 1;
+int sum = 0;
+int sum = 1;
+int sum = 0;
+int sum = 1;
+int sum = 0;
+int sum = 1;
+int sum = 0;
+int sum = 1;
+int sum = 0;
+int sum = 1;
+int sum = 0;
+int sum = 1;
+int sum = 0;
+int sum = 1;
+int sum = 0;
+int sum = 1;
+
+//Windows 10 transform Net hight TCP into Net ultra hight TCP
+int sum = 0;
+int sum = 1;
+int sum = 0;
+int sum = 1;
+int sum = 0;
+int sum = 1;
+int sum = 0;
+int sum = 1;
+int sum = 0;
+int sum = 1;
+int sum = 0;
+int sum = 1;
+int sum = 0;
+int sum = 1;
+int sum = 0;
+int sum = 1;
+int sum = 0;
+int sum = 1;
+
+//Windows 10 transform low framerate to hight framerate
+int sum = 0;
+int sum = 1;
+int sum = 0;
+int sum = 1;
+int sum = 0;
+int sum = 1;
+int sum = 0;
+int sum = 1;
+int sum = 0;
+int sum = 1;
+int sum = 0;
+int sum = 1;
+int sum = 0;
+
+//Windows 10 transform medium framerate to hight framerate
+int sum = 0;
+int sum = 1;
+int sum = 0;
+int sum = 1;
+int sum = 0;
+int sum = 1;
+int sum = 0;
+int sum = 1;
+int sum = 0;
+int sum = 1;
+
+//Windows 10 transform hight framerate to Ultra hight framerate
+int sum = 0;
+int sum = 1;
+int sum = 0;
+int sum = 1;
+int sum = 0;
+int sum = 1;
+int sum = 0;
+int sum = 1;
+int sum = 0;
+int sum = 1;
+int sum = 0;
+int sum = 1;
+int sum = 0;
+int sum = 1;
+
+//Windows 10 transform low fps to hight fps
+int sum = 0;
+int sum = 1;
+int sum = 0;
+int sum = 1;
+
+//Windows 10 transform medium fps to hight fps
+int sum = 0;
+int sum = 1;
+int sum = 0;
+int sum = 1;
+int sum = 0;
+int sum = 1;
+
+//Windows 10 transform hight fps to ultra hight fps
+int sum = 0;
+int sum = 1;
+int sum = 0;
+int sum = 1;
+int sum = 0;
+int sum = 1;
+int sum = 0;
+int sum = 1;
+
+//Windows 10 fixer waiting application
+int sum = 0;
+int sum = 1;
+int sum = 0;
+int sum = 1;
+int sum = 0;
+int sum = 1;
+int sum = 0;
+int sum = 1;
+int sum = 0;
+int sum = 1;
+
+//Windows 10 fixer Net waiting speed
+int sum = 0;
+int sum = 1;
+int sum = 0;
+int sum = 1;
+int sum = 0;
+int sum = 1;
+int sum = 0;
+int sum = 1;
+int sum = 0;
+int sum = 1;
+
+//Windows 10 fixer Net waiting ping and connection
+int sum = 0;
+int sum = 1;
+int sum = 0;
+int sum = 1;
+int sum = 0;
+int sum = 1;
+int sum = 0;
+int sum = 1;
+int sum = 0;
+int sum = 1;
+
+//Windows 10 transform Net low connection to Net hight connection
+int sum = 0;
+int sum = 1;
+int sum = 0;
+int sum = 1;
+
+//Windows 10 transform Net medium connection to Net hight connection
+int sum = 0;
+int sum = 1;
+int sum = 0;
+int sum = 1;
+int sum = 0;
+int sum = 1;
+
+//Windows 10 transform Net hight connection to Net ultra hight connection
+int sum = 0
+int sum = 1;
+int sum = 0;
+int sum = 1;
+int sum = 0;
+int sum = 1;
+int sum = 0;
+int sum = 1;
+int sum = 0;
+int sum = 1;
+
+//Windows 10 transform Net low connection sped to Net hight connection speed
+int sum = 0;
+int sum = 1;
+int sum = 0;
+int sum = 1;
+
+//Windows 10 transform Net medium connection speed to Net hight connection speed
+int sum = 0;
+int sum = 1;
+int sum = 0;
+int sum = 1;
+int sum = 0;
+int sum = 1;
+
+//Windows 10 transform Net hight connection speed to Net ultra hight connection speed
+int sum = 0;
+int sum = 1;
+int sum = 0;
+int sum = 1;
+int sum = 0;
+int sum = 1;
+int sum = 0;
+int sum = 1;
+int sum = 0;
+int sum = 1;
+
+//Windows 10 optimizing desktop
+int sum = 0;
+int sum = 1;
+int sum = 0;
+int sum = 1;
+int sum = 0;
+int sum = 1;
+int sum = 0;
+int sum = 1;
+int sum = 0;
+int sum = 1;
+
+//Windows 10 optimizing context and context menu
+int sum = 0;
+int sum = 1;
+int sum = 0;
+int sum = 1;
+int sum = 0;
+int sum = 1;
+int sum = 0;
+int sum = 1;
+int sum = 0;
+int sum = 1;
+
+//Windows 10 optimizing applications and applications performance
+int sum = 0;
+int sum = 1;
+int sum = 0;
+int sum = 1;
+int sum = 0;
+int sum = 1;
+int sum = 0;
+int sum = 1;
+int sum = 0;
+int sum = 1;
+
+//Windows 10 optimizing drivers and system drivers
+int sum = 0;
+int sum = 1;
+int sum = 0;
+int sum = 1;
+int sum = 0;
+int sum = 1;
+int sum = 0;
+int sum = 1;
+int sum = 0;
+int sum = 1;
+
+//Windows 10 infinite colors for screen
+int sum = 0;
+int sum = 1;
+int sum = 0;
+int sum = 1;
+int sum = 0;
+int sum = 1;
+int sum = 0;
+int sum = 1;
+int sum = 0;
+int sum = 1;
+
+//Windows 10 infinite performance
+int sum = 0;
+int sum = 1;
+int sum = 0;
+int sum = 1;
+int sum = 0;
+int sum = 1;
+int sum = 0;
+int sum = 1;
+int sum = 0;
+int sum = 1;
+
+//Windows 10 Net optimizing performance and infinite performance
+int sum = 0;
+int sum = 1;
+int sum = 0;
+int sum = 1;
+int sum = 0;
+int sum = 1;
+int sum = 0;
+int sum = 1;
+int sum = 0;
+int sum = 1;
+
+
+
+ 
+
+
+//Compiler
+int sum = 0;
+int sum = 1;
+int sum = 0;
+int sum = 1;
+int sum = 0;
+int sum = 1;
+int sum = 0;
+int sum = 1;
+
+//Interpreter
+int sum = 0;
+int sum = 1;
+int sum = 0;
+int sum = 1;
+int sum = 0;
+int sum = 1;
+int sum = 0;
+int sum = 1;
+
+//Hybrid
+int sum = 0;
+int sum = 1;
+int sum = 0;
+int sum = 1;
+int sum = 0;
+int sum = 1;
