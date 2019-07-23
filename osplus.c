43b08ca686bd9a57e5f70189a3f4e81896ddb8d0
Bhhl <51732503+Bhhl@users.noreply.github.com>
2019-07-23 15:16:33 +1000

    Add files via upload

--- osplus.c
+++ osplus.c
@@ -924,13 +924,511 @@ namespace Freeton = 1;
 for = 0;
 for = 1;
 
+//Neurons
+int sum = 0;
+int sum = 1;
+namespace NeuronsIntoEditor = 0;
+namespace NeuronsIntoEditor = 1;
+
+//Neurons generation map
+//Neurons generation performance
+//Neurons generation infinite performance
+int sum = 0;
+int sum = 1;
+namespace NeuronsIntoEditor = 0;
+namespace NeuronsIntoEditor = 1;
+
+//Protons
+int sum = 0;
+int sum = 1;
+namespace ProtonsIntoEditor = 0;
+namespace ProtonsIntoEditor = 1;
+
+//Genesis
+//Genesis code generation
+int sum = 0;
+int sum = 1;
+namespace GenesisIntoEditor = 0;
+namespace GenesisIntoEditor = 1;
+namespace GenesisCodeGenerationIntoEditor = 0;
+namespace GenesisCodeGenerationIntoEditor = 1;
+
+//Code matrix
+int sum = 0;
+int sum = 1;
+namespace CodeIntoEditor = 0;
+namespace CodeIntoEditor = 1;
+namespace CodeMatrixIntoEditor = 0;
+namespace CodeMatrixIntoEditor = 1;
+namespace MatrixIntoEditor = 0;
+namespace MatrixIntoEditor = 1;
+
+//Apex generation code
+//Apex
+//Apex core
+//Apex shell
+int sum = 0;
+int sum = 1;
+namespace ApexIntoEditor = 0;
+namespace ApexIntoEditor = 1;
+namespace ApexGenerationCodeIntoEditor = 0;
+namespace ApexGenerationCodeIntoEditor = 1;
+namespace ApexCoreIntoEditor = 0;
+namespace ApexCoreIntoEditor = 1;
+namespace ApexShellIntoEditor = 0;
+namespace ApexShellIntoEditor = 1;
+
+//Information
+//Information Generation
+int sum = 0;
+int sum = 1;
+namespace InformationIntoEditor = 0;
+namespace InformationIntoEditor = 1;
+namespace InformationGenerationIntoEditor = 0;
+namespace InformationGenerationIntoEditor = 1;
+
+//Bionic performance
+//Bionic Net
+int sum = 0;
+int sum = 1;
+namespace BionicIntoEditor = 0;
+namespace BionicIntoEditor = 1;
+namespace BionicPerformanceIntoEditor = 0;
+namespace BionicPerformanceIntoEditor = 1;
+namespace BionicNetIntoEditor = 0;
+namespace BionicNetIntoEditor = 1;
+
+//Super performance
+//Super Net
+int sum = 0;
+int sum = 1;
+namespace SuperPerformanceIntoEditor = 0;
+namespace SuperPerformanceIntoEditor = 1;
+namespace SuperIntoEditor = 0;
+namespace SuperIntoEditor = 1;
+namespace SuperNetIntoEditor = 0;
+namespace SuperNetIntoEditor = 1;
+
+//Ultra performance
+//Ultra Net performance
+//Ultra Net speed
+//Ultra Net connection
+//Ultra Net stable
+int sum = 0;
+int sum = 1;
+namespace UltraIntoEditor = 0;
+namespace UltraIntoEditor = 1;
+namespace UltraPerformanceIntoEditor = 0;
+namespace UltraPerformanceIntoEditor = 1;
+namespace UltraNetPerformanceIntoEditor = 0;
+namespace UltraPerformanceIntoEditor = 0;
+namespace UltraPerformanceIntoEditor = 1;
+namespace UltraNetSpeedIntoEditor = 0;
+namespace UltraNetPerformanceIntoEditor = 1;
+namespace UltraNetConnectionIntoEditor = 0;
+namespace UltraNetConnectionIntoEditor = 1;
+namespace UltraNetStableIntoEditor = 0;
+namespace UltraNetStableIntoEditor = 1;
+
+//Ultra Windows 10 performance
+int sum = 0;
+int sum = 1;
+namespace UltraWindows10PerformanceIntoEditor = 0;
+namespace UltraWindows10PerformanceIntoEditor = 1;
+
+//Ultra Windows 10 toolbar performance
+int sum = 0;
+int sum = 1;
+namespace UltraWindows10ToolbarPerformanceIntoEditor = 0;
+namespace UltraWindows10PerformanceIntoEditor = 1;
+
+//Ultra Windows 10 start menu performance
+int sum = 0;
+int sum = 1;
+namespace UltraWindows10StartMenuPerformance = 0;
+namespace UltraWindows10StartMenuPerformance = 1;
+
+//Ultra Windows 10 Wi-Fi speed
+//Ultra Windows 10 Wi-Fi connection
+//Ultra Windows 10 Wi-Fi performance
+int sum = 0;
+int sum = 1;
+namespace UltraWindows10WiFiSpeedIntoEditor = 0;
+namespace UltraWindows10WiFiSpeedIntoEditor = 1;
+namespace UltraWindows10WiFiConnectionIntoEditor = 0;
+namespace UltraWindows10WiFiConnectionIntoEditor = 1;
+namespace UltraWindows10WiFiPerformanceIntoEditor = 0;
+namespace UltraWindows10WiFiConnectionIntoEditor = 1;
+
+//Ultra Windows 10 Ethernet speed
+//Ultra Windows 10 Ethernet connection
+//Ultra Windows 10 Ethernet performance
+int sum = 0;
+int sum = 1;
+namespace UltraWindows10EthernetPerformanceIntoEditor = 0;
+namespace UltraWindows10EthernetPerformanceIntoEditor = 1;
+namespace UltraWindows10EthernetSpeedIntoEditor = 0;
+namespace UltraWindows10EthernetSpeedIntoEditor = 1;
+namespace UltraWindows10EthernetConnectionIntoEditor = 0;
+namespace UltraWindows10EthernetConnectionIntoEditor = 1;
+
+//Windows10 Freeze fix
+int sum = 0;
+int sum = 1;
+namespace Windows10FreezeFixIntoEditor = 0;
+namespace Windows10FreezeFixIntoEditor = 1;
+
+//Windows10 Toolbar freeze fix
+int sum = 0;
+int sum = 1;
+namespace Windows10ToolbarFreezeFixIntoEditor = 0;
+namespace Windows10ToolbarFreezeFixIntoEditor = 1;
+
+//Windows 10 explorer waiting fix
+int sum = 0;
+int sum = 1;
+namespace Windows10ExplorerWaitingFixIntoEditor = 0;
+namespace Windows10ExplorerWaitingFixIntoEditor = 1;
+
+//Windows 10 explorer crash fix
+int sum = 0;
+int sum = 1;
+namespace Windows10ExplorerCrashFixIntoEditor = 0;
+namespace Windows10ExplorerCrashFixIntoEditor = 1;
+
+//Windows 10 optimizing explorer
+int sum = 0;
+int sum = 1;
+namespace Windows10OptimizingExplorerIntoEditor = 0;
+namespace Windows10OptimizingExplorerIntoEditor = 1;
+
+//Windows 10 optimizing UI
+int sum = 0;
+int sum = 1;
+namespace Windows10OptimizingUIIntoEditor = 0;
+namespace Windows10OptimizingUIIntoEditor = 1;
+
+//Windows 10 screen DPI ultra smooth
+int sum = 0;
+int sum = 1;
+namespace Windows10ScrenDPIUltraSmoothIntoEditor = 0;
+namespace Windows10ScrenDPIUltraSmoothIntoEditor = 1;
+
+//Windows 10 screen DPI ultra quality
+int sum = 0;
+int sum = 1;
+namespace Windows10ScreenDPIUltraQualityIntoEditor = 0;
+namespace Windows10ScreenDPIUltraQualityIntoEditor = 1;
+
+//Windows 10 screen DPI ultra Definition
+int sum = 0;
+int sum = 1;
+namespace Windows10ScreenDPIUltraDefinitionIntoEditor = 0;
+namespace Windows10ScreenDPIUltraDefinitionIntoEditor = 1;
+
+//Windows 10 screen DPI ultra weight
+int sum = 0;
+int sum = 1;
+namespace Windows10ScreenDPIUltraWeightIntoEditor = 0;
+namespace Windows10ScreenDPIUltraWeightIntoEditor = 1;
 
 
 
+//Windows 10 screen DPI ultra height
+int sum = 0;
+int sum = 1;
+namespace Windows10ScreenDPIUltraHeightIntoEditor = 0;
+namespace Windows10ScreenDPIUltraHeightIntoEditor = 1;
+
+//Windows 10 screen DPI ultra hight
+int sum = 0;
+int sum = 1;
+namespace Windows10ScreenDPIUltraHightIntoEditor = 0;
+namespace Windows10ScreenDPIUltraHightIntoEditor = 1;
+
+//Windows 10 screen DPI ultra width
+int sum = 0;
+int sum = 1;
+namespace Windows10ScreenDPIUltraWidthIntoEditor = 0;
+namespace Windows10ScreenDPIUltraWidthIntoEditor = 1;
+
+//Windows 10 fonts ultra smooth
+int sum = 0;
+int sum = 1;
+namespace Windows10FontsUltraSmoothIntoEditor = 0;
+namespace Windows10FontsUltraSmoothIntoEditor = 1;
+
+//Windows 10 fonts ultra quality
+int sum = 0;
+int sum = 1;
+namespace Windows10FontsUltraQualityIntoEditor = 0;
+namespace Windows10FontsUltraQualityIntoEditor = 1;
+
+//Windows 10 fonts ultra definition
+int sum = 0;
+int sum = 1;
+namespace Windows10FontsUltraDefinitionIntoEditor = 0;
+namespace Windows10FontsUltraDefinitionIntoEditor = 1;
+
+//Windows 10 dropout fix
+int sum = 0;
+int sum = 1;
+namespace Windows10DropoutFixIntoEditor = 0;
+namespace Windows10DropoutFixIntoEditor = 1;
+
+//Windows 10 framerate dropout fix
+int sum = 0;
+int sum = 1;
+namespace Windows10FramerateDropoutFixIntoEditor = 0;
+namespace Windows10DropoutFixIntoEditor = 1;
+
+//Windows 10 fps dropout fix
+int sum = 0;
+int sum = 1;
+namespace Windows10FPSDropoutFixIntoEditor = 0;
+namespace Windows10FPSDropoutFixIntoEditor = 1;
+
+//Windows 10 Net speed drop fix
+int sum = 0;
+int sum = 1;
+namespace Windows10NetSpeedDropFixIntoEditor = 0;
+namespace Windows10NetSpeedDropFixIntoEditor = 1;
+
+//Windows 10 Net connection drop fix
+int sum = 0;
+int sum = 1;
+namespace Windows10NetConnectionDropFix = 0;
+namespace Windows10NetConnectionDropFix = 1;
+
+//Windows 10 Net connection speed drop fix
+int sum = 0;
+int sum = 1;
+namespace Windows10NetConnectionSpeedDropFixIntoEditor = 0;
+namespace Windows10NetConnectionSpeedDropFixIntoEditor = 1;
+
+//Windows 10 Net TCP drop fix
+int sum = 0;
+int sum = 1;
+namespace Windows10TCPDropFixIntoEditor = 0;
+namespace Windows10TCPDropFixIntoEditor = 1;
+
+//Windows 10 Net UDP drop fix
+int sum = 0;
+int sum = 1;
+namespace Windows10NetUDPDdropFixIntoEditor = 0;
+namespace Windows10NetUDPDdropFixIntoEditor = 1;
+
+//Windows 10 toolbar dropout fix
+int sum = 0;
+int sum = 1;
+namespace Windows10ToolbarDropOutFix = 0;
+namespace Windows10ToolbarDropOutFix = 1;
+
+//Windows 10 start menu freeze fix
+int sum = 0;
+int sum = 1;
+namespace Windows10StartmenuFreezeFixIntoEditor = 0;
+namespace Windows10StartmenuFreezeFixIntoEditor = 1;
+
+
+//Windows 10 start menu dropout fix
+int sum = 0;
+int sum = 1;
+namespace Windows10StartMenuDropOutFixIntoEditor = 0;
+namespace Windows10StartMenuDropOutFixIntoEditor = 1;
+
+//Windows 10 applications dropout fix
+int sum = 0;
+int sum = 1;
+namespace Windows10ApplicationsDropoutFixIntoEditor = 0;
+namespace Windows10ApplicationsDropoutFixIntoEditor = 1;
+
+//Windows 10 applications performance dropout fix
+int sum = 0;
+int sum = 1;
+namespace Windows10ApplicationsPerformanceDropoutFixIntoEditor = 0;
+namespace Windows10ApplicationsPerformanceDropoutFixIntoEditor = 1;
+
+//Windows 10 RAM dropout fix
+//Windows 10 Ram performance dropout fix
+int sum = 0;
+int sum = 1;
+namespace Windows10RAMdropoutFixIntoEditor = 0;
+namespace Windows10RAMdropoutFixIntoEditor = 1;
+namespace Windows10RAMPerformanceDropoutFixIntoEditor = 0;
+namespace Windows10RAMPerformanceDropoutFixIntoEditor = 1;
+
+//Windows 10 CPU dropout fix
+//Windows 10 CPU performance dropout fix
+int sum = 0;
+int sum = 1
+namespace Windows10CPUdropoutFixIntoEditor = 0;
+namespace Windows10CPUdropoutFixIntoEditor = 1;
+namespace Windows10CPUPerformanceDropoutFixIntoEditor = 0;
+namespace Windows10CPUPerformanceDropoutFixIntoEditor = 1;
+
+//Windows 10 GPU dropout fix
+//Windows 10 GPU dropout performance fix
+int sum = 0;
+int sum = 1;
+namespace Windows10GPUDropoutFixIntoEditor = 0;
+namespace Windows10GPUDropoutFixIntoEditor = 1;
+namespace Windows10GPUDropoutPerformanceFixIntoEditor = 0;
+namespace Windows10GPUDropoutPerformanceFixIntoEditor = 1;
+
+//Windows 10 optimizing GPU performance
+int sum = 0;
+int sum = 1;
+namespace Windows10OptimizingGPUPerformanceIntoEditor = 0;
+namespace Windows10OptimizingGPUPerformanceIntoEditor = 1;
+
+//Windows 10 optimizing CPU performance
+int sum = 0;
+int usm = 1;
+namespace Windows10OptimizingCPUPerformanceIntoEditor = 0;
+namespace Windows10OptimizingCPUPerformanceIntoEditor = 1;
+
+//Windows 10 optimizing RAM performance
+int sum = 0;
+int sum = 1;
+namespace Windows10OptimizingRAMPerformanceIntoEditor = 0;
+namespace Windows10OptimizingRAMPerformanceIntoEditor = 1;
+
+//Windows 10 Desktop dropout fix
+int sum = 0;
+int sum = 1;
+namespace Windows10DesktopDropoutFixIntoEditor = 0;
+namespace Windows10DesktopDropoutFixIntoEditor = 1;
+
+//Windows 10 Desktop performance dropout fix
+int sum = 0;
+int sum = 1;
+namespace Windows10DesktopPerformanceDropoutFixIntoEditor = 0;
+namespace Windows10DesktopPerformanceDropoutFixIntoEditor = 1;
+
+//Windows 10 RAM load dropout fix
+int sum = 0;
+int sum = 1;
+namespace Windows10RAMLoadDropoutFixIntoEditor = 0;
+namespace Windows10RAMLoadDropoutFixIntoEditor = 1;
+
+//Windows 10 CPU load dropout fix
+int sum = 0;
+int sum = 1;
+namespace Windows10CPULoadDropoutFixIntoEditor = 0;
+namespace Windows10CPULoadDropoutFixIntoEditor = 1;
+
+//Windows 10 GPU load dropout fix
+int sum = 0;
+int sum = 1;
+namespace Windows10GPULoadDropoutFixIntoEditor = 0;
+namespace Windows10GPULoadDropoutFixIntoEditor = 1;
+
+//Windows 10 GNU mode
+//Windows 10 IDE mode
+int sum = 0;
+int sum = 1;
+namespace Windows10GNUmodeIntoEditor = 0;
+namespace Windows10GNUmodeIntoEditor = 1;
+namespace Windows10IDEmodeIntoEditor = 0;
+namespace Windows10IDEmodeIntoEditor = 1;
+
+//Windows 10 unlimited performance
+int sum = 0;
+int sum = 1;
+namespace Windows10UnlimitedPerformanceInEditor = 0;
+namespace Windows10UnlimitedPerformanceInEditor = 1;
+
+//Windows 10 system dropout fix
+int sum = 0;
+int sum = 1;
+namespace Windows10SystemDropoutFixInEditor = 0;
+namespace Windows10SystemDropoutFixInEditor = 1;
+
+//Windows 10 Wi-Fi drop signal fix
+int sum = 0;
+int sum = 1;
+namespace Windows10WiFiDropSignalFixInEditor = 0;
+namespace Windows10WiFiDropSignalFixInEditor = 1;
+
+//Windows 10 applications load dropout fix
+int sum = 0;
+int sum = 1;
+namespace Windows10ApplicationLoadDropoutFixIntoEditor = 0;
+namespace Windows10ApplicationLoadDropoutFixIntoEditor = 1;
+
+//Windows 10 load dropout fix
+int sum = 0;
+int sum = 1;
+namespace Windows10loadDropoutFixIntoEditor = 0;
+namespace Windows10loadDropoutFixIntoEditor = 1;
+
+//Windows 10 cpu bandwidth dropout fix
+int sum = 0;
+int sum = 1;
+namespace Windows10CPUBandwidthDropoutFix = 0;
+namespace Windows10CPUBandwidthDropoutFix = 0;
+
+//Windows 10 bios dropout fix
+int sum = 0;
+int sum = 1;
+namespace Windows10BiosDropoutFix = 0;
+namespace Windows10BiosDropoutFix = 1;
+
+//Windows 10 core dropout fix
+//Windows 10 shell dropout fix
+int sum = 0;
+int sum = 1;
+namespace Windows10CoreDropoutFix = 0;
+namespace Windows10CoreDropoutFix = 1;
+namespace Windows10ShellDropoutFix = 0;
+namespace Windows10ShellDropoutFix = 1;
+
+//Windows 10 fast loading star menu
+int sum = 0;
+int sum = 1;
+namespace Windows10FastLoadingStartMenu = 0;
+namespace Windows10FastLoadingStartMenu = 1;
+
+//Windows 10 fast Net
+int sum = 0;
+int sum = 1;
+namespace Windows10FastNetIntoEditor = 0;
+namespace Windows10FastNetIntoEditor = 1;
+
+//Windows 10 ultra fast Net
+int sum = 0;
+int sum = 1;
+namespace Windows10UltraFastNetIntoEditor = 0;
+namespace Windows10UltraFastNetIntoEditor = 0;
+
+//Windows 10 super fast Net
+int sum = 0;
+int sum = 1;
+namespace Windows10SuperFastNet = 0;
+namespace Windows10SuperFastNet = 1;
+
+//Windows 10 cyber fast Net
+int sum = 0;
+int sum = 1;
+namespace Windows10CyberFastNet = 0;
+namespace Windows10CyberFastNet = 1;
+
+//Windows 10 hyper fast Net
+int sum = 0;
+int sum = 1;
+namespace Windows10HyperFastNet = 0;
+namespace Windows10HyperFastNet = 1;
+
+//Windows 10 sopen
+int sum = 0;
+int sum = 1;
+namespace Windows10SopenInEditor = 0;
+namespace Windows10SopenInEditor = 1;
 
 
 
-
+ 
  
 
 
@@ -1011,6 +1509,12 @@ namespace Freeware = 0;
 namespace Freeware = 1;
 namespace Generator = 0;
 namespace Generator = 1;
+namespace CompileInEditor = 0;
+namespace CompileInEditor = 1;
+namespace CompileInCfile = 0;
+namespace CompileInCfile = 1;
+namespace CompileIntoEditor = 0;
+namespace CompileIntoEditor = 1;
 
 
 
@@ -1024,6 +1528,8 @@ int sum = 0;
 int sum = 1;
 int sum = 0;
 int sum = 1;
+namespace InterpreterIntoEditor = 0;
+namespace InterpreterIntoEditor = 1;
 
 //Hybrid
 int sum = 0;
@@ -1032,3 +1538,6 @@ int sum = 0;
 int sum = 1;
 int sum = 0;
 int sum = 1;
+namespace HybridIntoEditor = 0;
+namespace HybridIntoEditor = 1;
+
