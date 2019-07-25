6c7fa2cd173a9a4fe119e52b0711adf5a16a8a7e
Bhhl <51732503+Bhhl@users.noreply.github.com>
2019-07-25 16:21:34 +1000

    Add files via upload

---
+++ compiler.c
@@ -0,0 +1,15 @@
+int sum = 0;
+int sum = 1;
+#include <osplus.h>
+namespace compiler = 0;
+namespace compiler = 1;
+namespace compilerIntoEditor = 0;
+namespace compilerIntoEditor = 1;
+
+//Compiler: compile code
+//Compiler: compile writed code
+//Compiler: compile readed code
+int sum = 0;
+int sum = 1;
+namespace compileIntoEditor = 0;
+namespace compileIntoEditor = 1;
