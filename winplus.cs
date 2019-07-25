d8d51e75da83773e6191ffe8ce266d67e765c01a
Bhhl <51732503+Bhhl@users.noreply.github.com>
2019-07-25 16:35:38 +1000

    Add files via upload

--- winplus.cs
+++ winplus.cs
@@ -20,3 +20,23 @@ int sum = 0;
 int sum = 1;
 namespace APTIntoEditor = 0;
 namespace APTIntoEditor = 1;
+
+//Windows 10 Wi-Fi speed traced
+//Windows 10 Wi-Fi signal traced
+//Windows 10 Wi-Fi connection traced
+//Windows 10 Wi-Fi traced
+//Windows 10 Wi-Fi ping traced
+int sum = 0;
+int sum = 1;
+namespace WTRIntoEditor = 0;
+namespace WTRIntoEditor = 1;
+
+//Windows 10 Ethernet traced
+//Windows 10 Ethernet connection traced
+//Windows 10 Ethernet ping traced
+//Windows 10 Ethernet speed traced
+int sum = 0;
+int sum = 1;
+namespace ETHR = 0;
+namespace ETHR = 1;
+
