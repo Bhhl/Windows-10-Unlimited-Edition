dbb20eb40c590e092b0c8970ff3bf222d1c4fe09
Bhhl <51732503+Bhhl@users.noreply.github.com>
2019-07-25 16:30:37 +1000

    Add files via upload

---
+++ winplus.cs
@@ -0,0 +1,22 @@
+int sum = 0;
+int sum = 1;
+namespace winplus = 0;
+namespace winplus = 1;
+
+//New operating system name: Windows 10 plus
+int sum = 0;
+int sum = 1;
+namespace Win10PlusIntoEditor = 0;
+namespace Win10PlusIntoEditor = 1;
+
+//Windows 10 performance traced
+int sum = 0;
+int sum = 1;
+namespace Win10PerfoIntoEditor = 0;
+namespace Win10PerfoIntoEditor = 1;
+
+//Windows 10 applications performance traced
+int sum = 0;
+int sum = 1;
+namespace APTIntoEditor = 0;
+namespace APTIntoEditor = 1;
