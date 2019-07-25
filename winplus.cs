9c14b60c9e300614114658ad36238b3a98897448
Bhhl <51732503+Bhhl@users.noreply.github.com>
2019-07-25 21:09:02 +1000

    Add files via upload

--- winplus.cs
+++ winplus.cs
@@ -37,6 +37,171 @@ namespace WTRIntoEditor = 1;
 //Windows 10 Ethernet speed traced
 int sum = 0;
 int sum = 1;
-namespace ETHR = 0;
-namespace ETHR = 1;
+namespace ETHRIntoEditor = 0;
+namespace ETHRIntoEditor = 1;
+
+//Windows 10 start menu traced
+int sum = 0;
+int sum = 1;
+namespace SMTIntoEditor = 0;
+namespace SMTIntoEditor = 1;
+
+//Windows 10 toolbar traced
+int sum = 0;
+int sum = 1;
+namespace = TLBRTIntoEditor = 0;
+namespace = TLBRTIntoEditor = 1;
+
+//Windows 10 start menu reflections
+int sum = 0;
+int sum = 1;
+namespace = SMRIntoEditor = 0;
+namespace = SMRIntoEditor = 1;
+
+//Windows 10 toolbar reflections 
+int sum = 0;
+int sum = 1;
+namespace TRIntoEditor = 0;
+namespace TRIntoEditor = 1;
+
+//Windows 10 start menu reflective specular
+int sum = 0;
+int sum = 1;
+namespace STSIntoEditor = 0;
+namespace STSIntoEditor = 1;
+
+//Windows 10 toolbar reflective specular
+int sum = 0;
+int sum = 1;
+namespace TRFIntoEditor = 0;
+namespace TRFIntoEditor = 1;
+
+//Windows 10 reflective fonts
+int sum = 0;
+int sum = 1;
+namespace RFIntoEditor = 0;
+namespace RFIntoEditor = 1;
+
+//Windows 10 reflective specular fonts
+int sum = 0;
+int sum = 1;
+namespace RSFIntoEditor = 0;
+namespace RSFIntoEditor = 1;
+
+//Windows 10 reflective for screen
+//Windows 10 specular reflective for screen
+int sum = 0;
+int sum = 1;
+namespace SANIntoEditor = 0;
+namespace SANIntoEditor = 1;
+namespace SFRIntoEditor = 0;
+namespace SFRIntoEditor = 1;
+
+//Windows 10 reflective for screen DPI
+//Windows 10 reflective specular for screen DPI
+int sum = 0;
+int sum = 1;
+namespace RFSDIntoEditor = 0;
+namespace RFSDIntoEditor = 1;
+namespace RFSRIntoEditor = 0;
+namespace RFSRIntoEditor = 1;
+
+//Windows 10 reflective contrast for screen
+//Windows 10 reflective contrast for screen DPI
+int sum = 0;
+int sum = 1;
+namespace RFSQIntoEditor = 0;
+namespace RFSQIntoEditor = 1;
+namespace SCJXIntoEditor = 0;
+namespace SCJXIntoEditor = 1;
+
+//Windows 10 reflective brightness for screen
+//Windows 10 reflective brightness for screen DPI
+int sum = 0;
+int sum = 1;
+namespace RBFSIntoEditor = 0;
+namespace RBFSIntoEditor = 1;
+namespace RBSFDIntoEditor = 0;
+namespace RBSFDIntoEditor = 1;
+
+//Windows 10 reflective tracing for screen
+//Windows 10 reflective tracing for screen DPI
+int sum = 0;
+int sum = 1;
+namespace RTFSIntoEditor = 0;
+namespace RTFSIntoEditor = 1;
+namespace RTFSDIntoEditor = 0;
+namespace RTFSDIntoEditor = 1;
+
+//Windows 10 directly light for screen
+//Windows 10 directly light for screen DPI
+int sum = 0;
+int sum = 1;
+namespace DRIIntoEditor = 0;
+namespace DRIIntoEditor = 1;
+
+//Windows 10 directly light tracing for screen
+//Windows 10 directly light tracing for screen DPI
+int sum = 0;
+int sum = 1;
+namespace DLTSIntoEditor = 0;
+namespace DLTSIntoEditor = 1;
+
+//Windows 10 HDR for screen
+//Windows 10 HDR for screen DPI
+//Windows 10 HDR tracing for screen DPI
+//Windows 10 HDR tracing for screen
+//Windows 10 Bloom for screen
+//Windows 10 Bloom for screen DPI
+//Windows 10 Bloom tracing for screen
+//Windows 10 Bloom tracing for screen DPI
+int sum = 0;
+int sum = 1;
+namespace HDRBTIntoEditor = 0;
+namespace HDRBTIntoEditor = 1;
+
+//Windows 10 screen tracing
+//Windows 10 screen DPI tracing
+int sum = 0;
+int sum = 1;
+namespace STDIntoEditor = 0;
+namespace STDIntoEditor = 1;
+
+//Windows 10 ray tracing for screen
+//Windows 10 ray tracing for screen DPI
+//Windows 10 rays tracing for screen DPI
+//Windows 10 rays tracing for screen
+//Windows 10 ray for screen
+//Windows 10 ray for screen DPI
+//Windows 10 rays for screen
+//Windows 10 rays for screen DPI
+int sum = 0;
+int sum = 0;
+namespace RESIntoEditor = 0;
+namespace RESIntoEditor = 1;
+
+//Windows 10 Trace reflections for screen
+//Windows 10 Trace reflections for screen DPI
+int sum = 0;
+int sum = 1;
+namespace TRFSIntoEditor = 0;
+namespace TRFSIntoEditor = 1;
+
+//Windows 10 Direct light trace for screen
+//Windows 10 Direct light trace for screen DPI
+int sum = 0;
+int sum = 1;
+namespace DLTIntoEditor = 0;
+namespace DLTIntoEditor = 1;
+
+//Windows 10 Trace of mediocre lighting for screen
+//Windows 10 Trace of mediocre lighting for screen DPi
+int sum = 0;
+int sum = 1;
+namespace TROSIntoEditor = 0;
+namespace TROSIntoEditor = 1;
+
+
+
+
 
