958dafbd35125362bd09925073e8167c5ba15727
Bhhl <51732503+Bhhl@users.noreply.github.com>
2019-09-03 15:40:33 +1000

    Add files via upload

---
+++ Windowsp.cpp
@@ -0,0 +1,81 @@
+using System.Programmer;
+using System.USBProgrammer;
+
+namespace Program
+{
+    struct Windowsp
+    {
+        /* Programmer */
+    };
+    {
+        //Windows programmer
+        void programmer;
+        public void programmer;
+        private public;
+        asm programmer;
+        public int;
+        public int night;
+        public int day;
+        return;
+        {
+            //Windows programmer USB
+            void usb_programmer;
+            public void usb_programmer;
+            private public;
+            asm usb_programmer;
+            public int;
+            public int night;
+            return;
+            {
+                //Windows programmer CPU performance
+                void cpu_programmer;
+                public int cpu_programmer;
+                private public;
+                asm cpu_programmer
+                goto cpu_programmer
+                void::Program:cpu_programmer
+                return;
+                {
+                    //Windows programmer GPU performance
+                    void::Program::GPU_performance
+                    asm GPU_performance
+                    int GPU_performance;
+                    virtual GPU_performance
+                    return;
+                    {
+                        //Windows programmer RAM performance
+                        void::Program::Ram_performance
+                        asm Ram_performance
+                        int Ram_performance;
+                        virtual GPU_performance
+                        return;
+                        {
+                            //Windows CPU hight performance = infinite best
+                            void::Program::CPU_hight_performance
+                            asm CPU_hight_performance
+                            int CPU_hight_performance;
+                            virtual CPU_hight_performance
+                            return;
+                            {
+                                //Windows GPU hight performance = infinite best
+                                void::Program::GPU_hight_performance
+                                asm GPU_hight_performance
+                                int GPU_hight_performance
+                                virtual GPU_hight_performance
+                                return;
+                                {
+                                  //Windows RAM hight performance = infinite best
+                                  void::Program::Ram_hight_performance
+                                  asm Ram_hight_performance
+                                  int Ram_hight_performance;
+                                  virtual Ram_hight_performance
+                                  return;
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
