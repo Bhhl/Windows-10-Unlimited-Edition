1f25e0f3e4bf17c41e21c798e0794e672da66e0d
Bhhl <51732503+Bhhl@users.noreply.github.com>
2020-01-11 12:40:08 +1000

    Add files via upload

---
+++ Windows.cs
@@ -0,0 +1,101 @@
+using Microsoft.CSharp;
+using MessageBox.Show;
+using Microsoft.JScript;
+using Microsoft.VisualBasic;
+using Microsoft.Vsa;
+using Microsoft.Win32;
+using System;
+using STAThread;
+using Show;
+using SortedDictionary;
+using SortedDictionary.Enumerator;
+using SortedDictionary.KeyCollection;
+using SortedDictionary.KeyCollection.Enumerator;
+using SortedDictionary.ValueCollection;
+using SortedDictionary.ValueCollection.Enumerator;
+using SortedList;
+using Stack;
+using Stack.Enumerator;
+using StreamWriter;
+using StringBuilder;
+using Queue;
+using Queue.Enumerator;
+using Enumerator;
+using EqualityComparer;
+using Exception;
+using TextWriter;
+using Thread.AllocateDataSlot;
+using Thread;
+using Thread.AllocateNamedDataSlot;
+using Thread.Sleep;
+using ICollection;
+using IComparer;
+using IDictionary;
+using IEnumerable;
+using IEnumerator;
+using IEqualityComparer;
+using IList;
+using Int16;
+using Int32;
+using Int64;
+using IntPtr;
+using AllocateDataSlot;
+using AllocateNamedDataSlot;
+using Array.BinarySearch;
+using Array;
+using Array.Clear;
+using Array.ConstrainedCopy;
+using Array.Copy;
+using Array.CreateInstance;
+using Array.Equals;
+using Array.IndexOf;
+using Array.LastIndexOf;
+using Array.ReferenceEquals;
+using Array.Reverse;
+using Array.Sort;
+
+
+///Windows optimized
+
+///WiFi signal optimized
+
+
+	///Windows system optimized
+
+
+
+///Windows WiFi signal custom = 50db
+///Windows drivers optimized
+///Windows fps custom = 999
+///Windows custom applications;
+
+///Internet OSI optimized
+///Internet OSI rate custom = infinite
+///Internet OSI transfer rate custom = infinite
+
+///Using Microsoft.Windows
+///Using Windows.WiFi
+///Using Windows.Internet
+///Using Windows.InternetOSI
+
+///WiFi signal ultra
+///WiFi signal quality ultra
+
+
+///WiFi signal hight
+
+///Internet OSI TCP custom = infinite
+///Internet OSI UDP custom = infinite
+///Internet OSI protocol optimized
+///Internet OSI TCP optimized
+///Internet OSI UDP optimized
+///Internet OSI rate optimized
+///Internet OSI transfer rate optimized
+///Internet OSI connection optimized
+///Internet OSI connection rate optimized
+///Internet OSI connection speed optimized
+///Internet OSI speed optimized
+///Internet OSI optimized all
+///WiFi optimized all 
+///Internet optimized All 
+///Windows optimized ALL
