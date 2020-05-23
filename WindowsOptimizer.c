e1391593fadba07bf96272a3fa2fb49f021b7e7d
Bhhl <51732503+Bhhl@users.noreply.github.com>
2020-05-23 13:47:02 +1000

    Add files via upload

---
+++ WindowsOptimizer.c
@@ -0,0 +1,92 @@
+//Windows clean CPU load mode 40%
+Cevent::CleanCpu
+
+//Windows Clean CPu
+CEvent::Cpu_Load_Fix
+Cevent::Cpu_optimize_resources
+
+tcode:
+
+low_load_ram = 40%:
+low_load_cpu = 40%:
+low_load_gpu = 40%:
+windows_black_screen_fix:
+windows_black_screen_fixed:
+windows_performance_optimized:
+
+auto_clean_ram = activated:
+auto_clean_cpu = activated:
+auto_clean_gpu = activated:
+
+#define Hight_ram_load_fix = 1 // [Hight RAM load fix [1 enabled] [0 disabled] ]
+#define GPU_OPTIMIZING_WORKS = 1 // [Gpu optimizing works = 1 [1 enabled] [0 disabled] ]
+#define CPU_OPTIMIZING_WORKS = 1 // [Gpu optimizing works = 1 [1 enabled] [0 disabled] ]
+#define CPU_LOAD_FIX = 1 // [Fix 100% CPU using load = 1 [1 enabled] [0 disabled] ]
+#define AUTO_CLEAN_CPU = 1 // [Auto clean CPU = 1 [1 enabled] [0 disabled] ]
+#define AUTO_CLEAN_SVCHOST = 1 // [Auto clean svchost = 1 [1 enabled] [0 disabled] ]
+#define SVCHOST = 0 // [svchost = 0 [1 enabled] [0 disabled] ]
+#define FULL_CPU_CLEAN = 1 // [CPU clean when filling out 100% = 1 [1 enabled] [0 disabled] ]
+#define MEDIUM_CPU_CLEAN = 1 // [CPU clean when filling out 80% = 1 [1 enabled] [0 disabled] ]
+#define CPU_CLEAN_O = 1  // [CPU clean when filling out 90% = 1 [1 enabled] [0 disabled] ]
+#define CPU_CLEAN_OE = 1 // [CPU clean when filling out 70% = 1 [1 enabled] [0 disabled] ]
+#define CPU_CLEAN_OZ = 1 // [CPU clean when filling out 60% = 1 [1 enabled] [0 disabled] ]
+#define CPU_CLEAN_OX = 1 // [CPU clean when filling out 50% = 1 [1 enabled] [0 disabled] ]
+#define CPU_CLEAN_OS = 1 // [CPU clean when filling out 40% = 1 [1 enabled] [0 disabled] ]
+#define CPU_CLEAN_A = 1 // [CPU clean when filling out 30% = 1 [1 enabled] [0 disabled] ]
+#define CPU_CLEAN_B = 1 // [CPU clean when filling out 20% = 1 [1 enabled] [0 disabled] ]
+#define CPU_CLEAN_C = 1 // [CPU clean when filling out 10% = 1 [1 enabled] [0 disabled] ]
+#define CPU_CLEAN_D = 1 // [CPU clean when filling out 0% = 1 [1 enabled] [0 disabled] ]
+#define RAM_CLEAN_A = 1 // [RAM clean when filling out 100% = 1 [1 enabled] [0 disabled] ]
+#define RAM_CLEAN_B = 1 // [RAM clean when filling out 90% = 1 [1 enabled] [0 disabled] ]
+#define RAM_CLEAN_C = 1 // [RAM clean when filling out 80% = 1 [1 enabled] [0 disabled] ]
+#define RAM_CLEAN_D = 1 // [RAM clean when filling out 70% = 1 [1 enabled] [0 disabled] ]
+#define RAM_CLEAN_S = 1 // [RAM clean when filling out 60% = 1 [1 enabled] [0 disabled] ]
+#define RAM_CLEAN_X = 1 // [RAM clean when filling out 50% = 1 [1 enabled] [0 disabled] ]
+#define RAM_CLEAN_Z = 1 // [RAM clean when filling out 40% = 1 [1 enabled] [0 disabled] ]
+#define RAM_CLEAN_Y = 1 // [RAM clean when filling out 30% = 1 [1 enabled] [0 disabled] ]
+#define RAM_CLEAN_G = 1 // [RAM clean when filling out 20% = 1 [1 enabled] [0 disabled] ]
+#define RAM_CLEAN_SA = 1 // [RAM clean when filling out 10% = 1 [1 enabled] [0 disabled] ]
+#define RAM_CLEAN_SU = 1 // [RAM clean when filling out 0% = 1 [1 enabled] [0 disabled] ]
+#define GENOME_CPU = 1 // [Ultra high CPU performance = 1 [1 enabled] [0 disabled] ]
+#define GENOME_RAM = 1 // [Ultra high RAM performance = 1 [1 enabled] [0 disabled] ]
+#define NO_LOAD_CPU = 1 // [Disable high CPU load using = 1 [1 enabled] [0 disabled] ]
+#define NO_LOAD_RAM = 1 // [Disable high RAM load using = 1 [1 enabled] [0 disabled] ]
+#define CONTEXT_MENU = 1 // [High performance for windows context menu = 1 [1 enabled] [0 disabled] ]
+#define START_MENU = 1 // [High performance for windows start menu = 1 [1 enabled] [0 disabled] ]
+#define EXPLORER = 1 // [High performance for Windows explorer = 1 [1 enabled] [0 disabled] ]
+#define CORE = 1 // [High performance for Windows Core = 1 [1 enabled] [0 disabled] ]
+#define SHELL = 1 // [High performance for Windows Shell = 1 [1 enabled] [0 disabled] ]
+#define DESKTOP = 1 // [High performance for Windows Desktop = 1 [1 enabled] [0 disabled] ]
+#define DESKTOP_FREEZE_FIX = 1 // [Fix freeze for Windows Desktop = 1 [1 enabled] [0 disabled] ]
+#define APPLICATIONS = 1 // [High performance for Windows applications = 1 [1 enabled] [0 disabled] ]
+#define CHROMIUM_C = 1 // [High performance for chromium = 1 [1 enabled] [0 disabled] ]
+#define CHROMIUM_A = 1 // [Chromium high net speed = 1 [1 enabled] [0 disabled] ]
+#define CHROMIUM_B = 1 // [Chromium high connection = 1 [1 enabled] [0 disabled] ]
+#define CHROMIUM_S = 1 // [Chromium high performance for tabs = 1 [1 enabled] [0 disabled] ]
+#define CHROMIUM_E = 1 // [Chromium high performance for websites = 1 [1 enabled] [0 disabled] ]
+#define CHROMIUM_H = 1 // [Chromium high resolution images = 1 [1 enabled] [0 disabled] ]
+#define CHROMIUM_V = 1 // [Chromium hight resolution video = 1 [1 enabled] [0 disabled] ]
+#define CHROMIUM_HI = 1 // [Chromium high quality text = 1 [1 enabled] [0 disabled] ]
+#define WiFi = 1 // [High performance for WiFI = 1 [1 enabled] [0 disabled] ]
+#define WIFIH = 1 // [WiFiMax technology = 1 [1 enabled] [0 disabled] ]
+#define WIFIX = 1 // [High speed for WiFi = 1 [1 enabled] [0 disabled] ]
+#define WIFIC = 1 // [High connection for WiFi = 1 [1 enabled] [0 disabled] ]
+#define WIFIP = 0 // [WiFi ping = 0 [1 enabled] [0 disabled] ]
+#define CHROMIUMP = 0 // [Chromium ping = 0 [1 enabled] [0 disabled] ]
+#define NETPing = 0 // [Net ping = 0 [1 enabled] [0 disabled] ]
+#define NETS = 1 // [Net high speed = 1 [1 enabled] [0 disabled] ]
+#define NETP = 1 // [Net high connection = 1 [1 enabled] [0 disabled] ]
+#define HOSTS = 0 // [Host = 0 [1 enabled] [0 disabled] ]
+#define HOTSC = 1 // [Hosts = 0 [1 enabled] [0 disabled] ]
+#define IPVL = 1 // [IP level 8 = 1 [1 enabled] [0 disabled] ]
+#define IPVVI = 1 // [IP version 8 = 1 [1 enabled] [0 disabled] ]
+#define IPO = 1 // [IP high performance = 1 [1 enabled] [0 disabled] ]
+#define IPX = 1 // [IP high speed = 1 [1 enabled] [0 disabled] ]
+#define IPY = 1 // [IP high connection = 1 [1 enabled] [0 disabled] ]
+#define IPP = 0 // [IP ping = 0 [1 enabled] [0 disabled] ]
+#define NO_LOAD_GPU = 1 // Disable hight GPU load using = 1 [1 enabled] [0 disabled] ]
+#define GENOME_GPU = 1 // High performance for GPU = 1 [1 enabled] [0 disabled] ]
+#define GENOME_NET = 1 // High performance for NET = 1 [1 enabled] [0 disabled] ]
+#define NO_LOAD_NET = 1 // Disable hight Net load using = 1 [1 enabled] [0 disabled] ]
+
+
